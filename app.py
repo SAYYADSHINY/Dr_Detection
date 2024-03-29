5d17fb2cd79373e835f03dd3aa4ad14f51c71652
pavan kumar <20131a05h9@gvpce.ac.in>
2024-03-29 07:40:38 +0000

    new

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+import uvicorn
+from fastapi import FastAPI, Request, File, UploadFile,Form
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import requests
+import psycopg2
+
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-UPLOAD_FOLDER = "static"
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
 
 @app.get('/')
 def index(request: Request):
-    return templates.TemplateResponse("open.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get('/index')
 def index(request: Request):
